1fd2e42271dab2481abb15038576936ed6815a13
Matt Telker <mtelker@stevens.edu>
2017-02-15 20:01:05 -0500

    fully working, needs testing

--- CS546Lab4/app.js
+++ CS546Lab4/app.js
@@ -1,13 +1,61 @@
 const todoItems = require("./todo");
 
-//let createdTask = todoItems.createTask("My First Task", "This is the first thing I need to do today");
-let allTasks = todoItems.getAllTasks();
-//let removeTask = todoItems.removeTask("cdf66e53-883c-47f9-afe8-6b6c699cf2b6");
-let completeTask = todoItems.completeTask("41316e49-c696-4582-8664-bef7a4342d29");
-/*createdTask.then((task) => {
-    console.log(task);
-});*/
+// let createdTask = todoItems.createTask("Ponder Dinosaurs", "Has Anyone Really Been Far Even as Decided to Use Even Go Want to do Look More Like?");
+// let secondTask = todoItems.createTask("Play Pokemon with Twitch TV", "Should we revive helix?");
+// let allTasks = todoItems.getAllTasks();
+// let completeTask = todoItems.completeTask("41316e49-c696-4582-8664-bef7a4342d29");
+// let getTask = todoItems.getTask("41316e49-c696-4582-8664-bef7a4342d29");
 
+let FirstID, secondTask;
+
+todoItems.createTask("Ponder Dinosaurs", "Has Anyone Really Been Far Even as Decided to Use Even Go Want to do Look More Like?")
+        .then((task) => {
+            FirstID = task._id;
+            console.log(task);
+
+            return todoItems.createTask("Play Pokemon with Twitch TV", "Should we revive helix?");
+        })
+        .then((task) => {
+            SecondID = task._id;
+            
+            return todoItems.getAllTasks()
+        })
+        .then((taskArray) => {
+            console.log(taskArray);
+
+            return todoItems.removeTask(FirstID);
+        })
+        .then((id) => {
+            console.log("Removed task with id " + FirstID);
+
+            return todoItems.clearTasks();
+        })
+        .then((count) => {
+            console.log("Removed " + count + " entries.");
+        });
+
+
+
+// createdTask.then((task) => {
+//     let FirstID = task._id;
+//     console.log(task);
+// }).then(function() {
+//     return secondTask
+// })
+
+// secondTask.then((task) => {
+//     let SecondID = task._id;
+// });
+
+// allTasks.then((task) => {
+//     console.log(task);
+// }); 
+
+// let removeTask = todoItems.removeTask(FirstID);
+
+// removeTask.then(() => {
+//     console.log("Removed task with id" + FirstID);
+// });
 // let tryToGetTask = removeTask.then(() => {
 //     return todoItems.getTask("cdf66e53-883c-47f9-afe8-6b6c699cf2b6");
 // });
@@ -17,14 +65,24 @@ let completeTask = todoItems.completeTask("41316e49-c696-4582-8664-bef7a4342d29"
 //     console.error(error);
 // });
 
-completeTask.then((task) => { 
-   console.log(task);
-});
+
+
+// getTask.then((task) => {
+//     console.log("get task below");
+//     console.log(task);
+// });
+
+// completeTask.then((task) => { 
+//     console.log("completed task below");
+//    console.log(task);
+// });
 
 /*
 removeTask.then((task) => {
     console.log(task);
 }); */
-allTasks.then((task) => {
-    console.log(task);
-}); 
+
+
+// todoItems.clearTasks().then((count) => {
+//     console.log(count);
+// });
--- CS546Lab4/todo.js
+++ CS546Lab4/todo.js
@@ -1,3 +1,8 @@
+/* Matt Telker
+ * CS 546
+ * Lab 4
+ */
+
 const mongoCollections = require("./mongoCollections");
 const todoItems = mongoCollections.todoItems;
 const uuidV4 = require('uuid/v4');
@@ -10,7 +15,15 @@ let exportedMethods = {
         
         return todoItems().then((todoCollection) => {
             return todoCollection.findOne({_id: id});
-        });
+        })
+        .then((result) => {
+            if (result === null){
+                return Promise.reject(`No todo with id ${id} was found.`);
+            }
+            else {
+                return result;
+            }
+        })
     },
 
 
@@ -52,15 +65,20 @@ let exportedMethods = {
         
         return todoItems().then((todoCollection) => {
             let date = Date();
-            return todoCollection.update(
+            return todoCollection
+                .updateOne(
                 { _id: taskId },
                 { $set: 
                     {
                         complete: true,
                         completedAt: date
                     }
-                }
-            );
+                })
+                .then((updateInfo) => {
+                    if (updateInfo.modifiedCount === 0){
+                        return Promise.reject(`Could not update task with id of ${id}`);
+                    }
+            });
         });
     },
 
@@ -77,7 +95,21 @@ let exportedMethods = {
                     }
                 });
         });
+    },
+
+    clearTasks() {
+        return todoItems().then((todoCollections) => {
+            return todoCollections
+                .deleteMany({ })
+                .then((deletionInfo) => {
+                    if (deletionInfo.deletedCount === 0){
+                        return Promise.reject("Nothing to delete");
+                    }
+                    else 
+                        return deletionInfo.deletedCount;
+                })
+            });
+        }
     }
-}
 
 module.exports = exportedMethods;
