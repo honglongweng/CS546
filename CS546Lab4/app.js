a78839125ff41b96be9925b88036ef7ebc701d1d
Matt Telker <mtelker@stevens.edu>
2017-02-16 22:17:10 -0500

    commented, catches added

--- CS546Lab4/app.js
+++ CS546Lab4/app.js
@@ -1,12 +1,11 @@
+/*
+ * Matt Telker
+ * CS 546
+ * Lab 4
+ */
+
 const todoItems = require("./todo");
-
-// let createdTask = todoItems.createTask("Ponder Dinosaurs", "Has Anyone Really Been Far Even as Decided to Use Even Go Want to do Look More Like?");
-// let secondTask = todoItems.createTask("Play Pokemon with Twitch TV", "Should we revive helix?");
-// let allTasks = todoItems.getAllTasks();
-// let completeTask = todoItems.completeTask("41316e49-c696-4582-8664-bef7a4342d29");
-// let getTask = todoItems.getTask("41316e49-c696-4582-8664-bef7a4342d29");
-
-let FirstID, secondTask;
+let FirstID, SecondID;
 
 todoItems.createTask("Ponder Dinosaurs", "Has Anyone Really Been Far Even as Decided to Use Even Go Want to do Look More Like?")
         .then((task) => {
@@ -17,24 +16,42 @@ todoItems.createTask("Ponder Dinosaurs", "Has Anyone Really Been Far Even as Dec
         })
         .then((task) => {
             SecondID = task._id;
-            
-            return todoItems.getAllTasks()
+
+            return todoItems.getAllTasks();
         })
         .then((taskArray) => {
             console.log(taskArray);
 
             return todoItems.removeTask(FirstID);
         })
+        .then(() => {
+            
+            return todoItems.getAllTasks();
+        })
+        .then((taskArray) => {
+            console.log(taskArray);
+
+            return todoItems.completeTask(SecondID);
+        })
         .then((id) => {
-            console.log("Removed task with id " + FirstID);
+
+            return todoItems.getTask(SecondID);
+        })
+        .then((id) => {
+            console.log(id);
 
             return todoItems.clearTasks();
         })
         .then((count) => {
-            console.log("Removed " + count + " entries.");
+            console.log("This should be 1, signifying the one remaining task: " + count);
         });
 
 
+// let createdTask = todoItems.createTask("Ponder Dinosaurs", "Has Anyone Really Been Far Even as Decided to Use Even Go Want to do Look More Like?");
+// let secondTask = todoItems.createTask("Play Pokemon with Twitch TV", "Should we revive helix?");
+// let allTasks = todoItems.getAllTasks();
+// let completeTask = todoItems.completeTask("41316e49-c696-4582-8664-bef7a4342d29");
+// let getTask = todoItems.getTask("41316e49-c696-4582-8664-bef7a4342d29");
 
 // createdTask.then((task) => {
 //     let FirstID = task._id;
--- CS546Lab4/todo.js
+++ CS546Lab4/todo.js
@@ -9,6 +9,7 @@ const uuidV4 = require('uuid/v4');
 
 
 let exportedMethods = {
+    /* This function returns a promise that resolves to a task from the database, when given an id */
     getTask(id) {
         if (!id) 
             return Promise.reject("You must provide an id to search for");
@@ -27,6 +28,7 @@ let exportedMethods = {
     },
 
 
+    /* This function returns a promise that resolves to a newly created to-do list object */
     createTask(title, description) {
         if (!title)
             return Promise.reject("You must provide a title for your task.");
@@ -49,16 +51,22 @@ let exportedMethods = {
                 })
                 .then((newId) => {
                     return this.getTask(newId);
+                })
+                .catch(function (message){
+                    return Promise.reject("Could not insert item due to: " + message);
                 });
         });
     },
 
+    /* This function returns a promise that resolves to an array of all tasks in the database. */
     getAllTasks() {
         return todoItems().then((todoCollections) => {
             return todoCollections.find().toArray();
         });
     },
 
+    /* This function modifies the given task in the database. 
+     * It sets completed to true and completedAt to the current time */
     completeTask(taskId) {
         if (!taskId) 
             return Promise.reject("You must provide an id to complete");
@@ -78,10 +86,14 @@ let exportedMethods = {
                     if (updateInfo.modifiedCount === 0){
                         return Promise.reject(`Could not update task with id of ${id}`);
                     }
-            });
+                })
+                .catch(function(message){
+                    return Promise.reject("Could not complete task due to: " + message);
+                });
         });
     },
 
+    /* This function removes the given task from the database. */
     removeTask(id) {
         if (!id) 
             return Promise.reject("You must provide an id to search for");
@@ -93,10 +105,15 @@ let exportedMethods = {
                     if (deletionInfo.deletedCount === 0) {
                         return Promise.reject(`Could not delete task with id of ${id}`);
                     }
+                })
+                .catch(function(message){
+                    return Promise.reject("Could not remove task due to error: " +message);
                 });
         });
     },
 
+    /* This function was not required by the assignment, but was useful for testing.
+     * It deletes everything, and returns the count of deleted items from the database. */
     clearTasks() {
         return todoItems().then((todoCollections) => {
             return todoCollections
